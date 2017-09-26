c29bc42ab74e001cfa3b520aa49b60dc69f5c94b
waqaramjad <waqaramjad345@gmail.com>
2017-09-26 21:32:49 +0500

    Sign Up login

---
+++ public/Js/SignUp&SigIn.js
@@ -0,0 +1,77 @@
+
+var database = firebase.database().ref('/');
+var firstName = document.getElementById('firstname')
+var LastName = document.getElementById('lastname')
+var email = document.getElementById('email')
+var password = document.getElementById('passwd')
+
+// loginform
+
+var LogEmail = document.getElementById('login-username')
+var logPass = document.getElementById('login-password')
+
+document.getElementById('loginform')
+    .addEventListener("submit", function (event) {
+        event.preventDefault()
+        var user = {
+            email: LogEmail.value,
+            password: logPass.value
+        }
+
+        firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+            .then(function (res) {
+                console.log(res.uid)
+                database.child('users/' + res.uid)
+                    .once('value', function (snapshot) {
+                        var convert = JSON.stringify(snapshot.val())
+                        localStorage.setItem('loggedInUser', convert)
+                        location = 'Quiz/Quiz.html'
+                        console.log(convert)
+                    })
+            })
+            .catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                if (errorCode === 'auth/wrong-password') {
+                    alert('Wrong password.');
+                } else {
+                    alert(errorMessage);
+                }
+                console.log(error);
+            });
+
+    })
+
+//  sign up form
+function submit1()
+{
+  
+    var user = {
+
+        fName: firstName.value,
+        LName: LastName.value,
+        email:email.value,
+        pass:password.value
+    }
+    
+    firebase.auth().
+    createUserWithEmailAndPassword(user.email,user.pass)
+    .then(function (res) {
+        console.log(res)
+        database.child('users/' + res.uid).set(user)
+    })
+
+    .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
+    });
+
+}
---
+++ public/Js/firebase.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAMOScq5hEh484m2ISI4udr2epqmM0t9m4",
+    authDomain: "quiz-application-fiver.firebaseapp.com",
+    databaseURL: "https://quiz-application-fiver.firebaseio.com",
+    projectId: "quiz-application-fiver",
+    storageBucket: "quiz-application-fiver.appspot.com",
+    messagingSenderId: "439055475610"
+  };
+  firebase.initializeApp(config);
+
